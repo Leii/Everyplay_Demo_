6fc27ab5f6edf895a9d7e65a168507cef1f536a5
Leii <zzlthor@gmail.com>
2015-06-30 20:24:45 +0800

    project
    An integrated Everyplay Unity project

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour {
+
+    public Button btn_play;
+    public Button btn_stop;
+    public Button btn_replay;
+    public Text stateTxt;
+    private float pos_z;
+    bool isMove;
+
+	// Use this for initialization
+	void Start () {
+        isMove = true;
+        pos_z = this.transform.position.z;
+      
+        if (Everyplay.IsSupported()&&Everyplay.IsRecordingSupported())
+        {
+            stateTxt.text = "is supported";
+        }
+
+        btn_play.onClick.AddListener(delegate()
+        {
+            Debug.Log("Start Recording!");
+            Everyplay.SetMetadata("score",pos_z);
+            if (Everyplay.IsRecording())
+            {
+                stateTxt.text = "is recording";
+            }else
+                stateTxt.text = "error";
+            
+            Everyplay.StartRecording();
+        });
+        btn_stop.onClick.AddListener(delegate()
+        {
+            stateTxt.text = "stop recording";
+            Everyplay.StopRecording();
+            Debug.Log("Stop Recording!");
+        });
+        btn_replay.onClick.AddListener(delegate()
+        {
+            Everyplay.PlayLastRecording();
+            isMove = false;
+            Debug.Log("play last recording");
+           
+        });
+
+	}
+   
+	// Update is called once per frame
+	void Update () {
+        if (isMove)
+        {
+            this.transform.Translate(new Vector3(0, 0, -0.01f));
+        }
+       
+	}
+}
---
+++ Assets/Shaders/EarthShader.shader
@@ -0,0 +1,56 @@
+﻿Shader "Custom/EarthShader" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Cloud ("Base2 (RGB)",2D) = "white" {}
+		_Color ("color",Color) = (1,1,1,0.5)
+	}
+	SubShader {
+		Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+		LOD 200
+		Pass{
+		CGPROGRAM
+		#pragma vertex vert
+		#pragma fragment frag
+		#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+		sampler2D _Cloud;
+		
+		float4 _Color;
+
+		struct v2f {
+		    float4 pos: SV_POSITION;
+			float2 uv: TEXCOORD0;
+		};
+		float4 _MainTex_ST;
+		
+		v2f vert (appdata_base v)
+		{
+		    v2f o;
+		    o.pos = mul(UNITY_MATRIX_MVP,v.vertex);
+		    o.uv = TRANSFORM_TEX(v.texcoord,_MainTex);
+		    return o;
+		} 
+		half4 frag(v2f i) : COLOR
+		{
+		    float u_x = i.uv.x + -0.2*_Time;
+		    float2 uv_earth = float2(u_x,i.uv.y);
+		    half4 texcolor_earth = tex2D (_MainTex,uv_earth);
+		    //
+		    float2 uv_cloud;
+		    u_x = i.uv.x + -0.3*_Time;
+		    uv_cloud = float2(u_x,i.uv.y);
+		    half4 tex_cloudDepth = tex2D (_Cloud,uv_cloud);
+		    //white * depth 
+		    half4 texcolor_cloud = float4(1,1,1,0)*(tex_cloudDepth.x);
+		    
+		    //combine earth with cloud
+		    return lerp(texcolor_earth,texcolor_cloud,0.5f);
+		
+		}
+		
+		ENDCG
+		}
+	} 
+//	FallBack "Diffuse"
+}
